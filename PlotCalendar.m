27dbbdae69e95e4257cdc5094e83bc99210bbe50
kmisiunas <k.misiunas@gmail.com>
2016-12-29 00:23:45 +0200

    first commit

---
+++ PlotCalendar.m
@@ -0,0 +1,22 @@
+(* Mathematica Package *)
+(* Created by Mathematica Plugin for IntelliJ IDEA *)
+
+(* :Title: PlotCalendar *)
+(* :Context: PlotCalendar` *)
+(* :Author: kmisiunas *)
+(* :Date: 2016-12-29 *)
+
+(* :Package Version: 0.1 *)
+(* :Mathematica Version: *)
+(* :Copyright: (c) 2016 kmisiunas *)
+(* :Keywords: *)
+(* :Discussion: *)
+
+BeginPackage["PlotCalendar`"]
+(* Exported symbols added here with SymbolName::usage *)
+
+Begin["`Private`"]
+
+End[] (* `Private` *)
+
+EndPackage[]
